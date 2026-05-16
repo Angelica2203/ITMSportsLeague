3c622a1046817741999569e69695d641b2b10bf8
jugato <julianricardo_21@hotmail.com>
2026-05-15 19:10:17 -0500

    Phase 6 completed - Standings and Stats

---
+++ SportsLeague.Domain/Interfaces/Services/IStandingsService.cs
@@ -0,0 +1,10 @@
+﻿namespace SportsLeague.Domain.Interfaces.Services
+{
+    public interface IStandingsService
+    {
+        Task<object> GetStandingsAsync(int tournamentId);//Obtener la tabla de posiciones del torneo
+        Task<object> GetTopScorersAsync(int tournamentId); //Obtener los máximos goleadores del torneo
+        Task<object> GetCardStatsAsync(int tournamentId); //Obtener las estadísticas de tarjetas (amarillas y rojas) por jugador en el torneo
+    }
+
+}
